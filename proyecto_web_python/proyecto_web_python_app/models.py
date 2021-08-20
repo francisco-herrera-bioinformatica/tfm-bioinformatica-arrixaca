d521fcacfa212f853d30bf3ec21d21791ae7d010
Fran <fjherrera@ucam.edu>
2021-08-20 12:00:27 +0200

    Arreglada subida de ficheros

--- proyecto_web_python/proyecto_web_python_app/models.py
+++ proyecto_web_python/proyecto_web_python_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Documento(models.Model):
+    nombre = models.CharField(max_length=256)
+    extension = models.CharField(max_length=20)
+    longitud = models.CharField(max_length=50)
+    fichero = models.FileField()
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name='documento'
+        verbose_name_plural='documentos'
+
+    def __str__(self):
+        return self.nombre
